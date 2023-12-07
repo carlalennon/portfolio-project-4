1833d7db6070dbe4cd1395541c2d2de63243ad2a
carlalennon <99259113+carlalennon@users.noreply.github.com>
2023-12-07 21:34:00 +0000

    Add render template req for login.html

--- PP4/views.py
+++ PP4/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
+def loginRequest(request):
+    return render(request, 'gknit/login.html')
