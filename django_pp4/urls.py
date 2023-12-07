84db51a757a22caaec1e9117d9297e9788c9dabb
carlalennon <99259113+carlalennon@users.noreply.github.com>
2023-12-07 21:38:32 +0000

    Set up login page url

--- django_pp4/urls.py
+++ django_pp4/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views import loginRequest
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', loginRequest, name='login')
 ]
