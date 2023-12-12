96178446da3b0ce298e8769aa8a1758ff3f6a03a
carlalennon <99259113+carlalennon@users.noreply.github.com>
2023-12-12 21:03:49 +0000

    Delete extra django project

--- PP4/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Pp4Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PP4'
--- PP4/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def loginRequest(request):
-    return render(request, 'gknit/login.html')
