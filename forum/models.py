4b6821a16317e4e826bcedfaa35b8394ed693609
carlalennon <>
2024-01-07 00:47:40 +0000

    Add ER table to forum/models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+## Forum model - Board ER table - * = Forgein key
+# _________________
+#|_ID______________|      
+#|_Thread Name_____|
+#|_Creator_________|*
+#|_Date____________|
+#|_no. Replies_____|
+#|_Pinned status___|
+#|_Tags____________|
+#|_Content_________|
+#|_Time____________|
+#|_no. Views_______|
+#|_Replies_________|
