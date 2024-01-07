e0d5331866fddfd66cb5d7b54b7ef97c5474e16d
carlalennon <>
2024-01-07 01:00:03 +0000

    Testing thread in database

--- forum/models.py
+++ forum/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 ## Forum model - Board ER table - * = Forgein key
@@ -6,6 +7,8 @@ from django.db import models
 #|_ID______________|      
 #|_Thread Name_____|
 #|_Creator_________|*
+#|_Creator icon____|
+#|_Creator bio_____|
 #|_Date____________|
 #|_no. Replies_____|
 #|_Pinned status___|
@@ -14,3 +17,20 @@ from django.db import models
 #|_Time____________|
 #|_no. Views_______|
 #|_Replies_________|
+
+# Much of this is lifted from the LMS
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Thread(models.Model):
+    #Thread title
+    title = models.CharField(max_length=500, unique=True)
+    #Creates url for post - replace with number in future
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    # Text post
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
--- gknit/settings.py
+++ gknit/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello_world',
+    'forum',
 ]
 
 MIDDLEWARE = [
