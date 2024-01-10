24aee0cfea240072b2c84a82fad0804375879f9e
carlalennon <>
2024-01-10 22:31:42 +0000

    Add model content to about section

--- about/models.py
+++ about/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# ER About site 
+
+# _______________
+#|_ Title________|
+#|_Admin icon____|  
+#|_Admin name____|
+#|_About content_|
+#|_Date created__|
+#|_______________|
+
+
+class About(models.Model):
+    title = models.Charfield(max_length=300, unique=False)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="about_site"
+    )
+    # Icon for user
+    # Content
+    content = models.TextField()
+    # Date and Time
+    created_on = models.DateTimeField(auto_now_add=True)
